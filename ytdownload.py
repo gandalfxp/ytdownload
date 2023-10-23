e3fa20de78661c951178bc19c938ce8651df8089
theohoppe3 <theohoppe3@gmail.com>
2023-10-23 16:53:52 +0200

    now saving as mp3, fixed mkdir errors

--- ytdownload.py
+++ ytdownload.py
@@ -3,7 +3,7 @@ from pytube import Playlist, YouTube
 
 def on_progress(stream, chunk: bytes, bytes_remaining: int) -> None:
     bytes_downloaded = stream.filesize - bytes_remaining
-    progress = 100 / stream.filesize * bytes_downloaded
+    progress = round(100 / stream.filesize * bytes_downloaded, 1)
     print(f'⬇️ [{progress}%] {stream.title}', end='\r')
 
 def on_complete(stream, filepath: str) -> None:
@@ -12,12 +12,9 @@ def on_complete(stream, filepath: str) -> None:
 if __name__ == '__main__':
     url = 'https://www.youtube.com/playlist?list=PLOzDu-MXXLlgKMA3S2FXY_XVyDrukc0si'
     playlist = Playlist(url)
-    output_path = f'./{playlist.title}'
-
-    mkdir(output_path)
 
     for url in playlist.video_urls:
         video = YouTube(url)
         video.register_on_progress_callback(on_progress)
         video.register_on_complete_callback(on_complete)
-        video.streams.get_audio_only().download(output_path)
+        video.streams.get_audio_only().download(f'./{playlist.title}', f'{video.title}.mp3')
